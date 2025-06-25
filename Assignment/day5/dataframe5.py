f5c80d00a28dd280351ed3f7fdb256dd134d19f7
Hsay27 <yashbansal1007@gmail.com>
2025-06-26 02:44:08 +0530

    update

--- Assignment/day5/dataframe5.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd 
-data = [
-    {'ID': 1, 'Name': 'sanidhya', 'Score': 85},
-    {'ID': 2, 'Name': 'shivam ','Score': 90},
-    {'ID': 3, 'Name': 'luck', 'Score': 95}
-]
-
-df = pd.DataFrame(data)
-print(df)
